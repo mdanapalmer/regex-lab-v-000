b2082d00209dfa829f7d52a26f668a22ba1cab01
Dana Palmer <roiai4@hotmail.com>
2018-07-03 02:19:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,10 @@ end
 
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
